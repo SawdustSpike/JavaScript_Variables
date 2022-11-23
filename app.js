c6c2730705c603f9540eac7bdda285a637239ff3
Mike Cowell <spike@spikeandbetty.com>
2022-11-22 21:24:42 -0500

    init commit

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-console.log("Hello World!\n==========\n");
-console.log(
-  "Follow the steps in the README.md file to complete the exercises:\n==========\n"
-);
+const firstName = "Michael";
+let lastName = "Cowell";
+var age = 40;
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
 
-// Exercise 1
-
-// Exercise 2
-
-// Exercise 3
